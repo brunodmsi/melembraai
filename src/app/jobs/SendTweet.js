e519efc52962a41f446a622a81d008ceed218adb
Bruno De Masi <brunodemasi1@gmail.com>
2020-04-01 20:51:50 -0400

    fix: fixed params

--- src/app/jobs/SendTweet.js
+++ src/app/jobs/SendTweet.js
@@ -6,12 +6,12 @@ class SendTweet {
   }
 
   async handle({ data }) {
-    const { date, reminder } = data;
+    const { reminder, message } = data;
 
     await TweetHandler.replyTweet({
       username: reminder.requester.name,
       tweet_id: reminder.tweet.id,
-      message: `Ok! Seu lembrete foi agendado para ${date}! Até lá!`,
+      message,
     });
   }
 }
