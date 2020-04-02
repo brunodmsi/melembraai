b07cbb2db60cc94cc9234f3ae2ed13aee94bc65f
Bruno De Masi <brunodemasi1@gmail.com>
2020-04-02 08:40:10 -0400

    feat: check for reminders; project finished

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ setInterval(async function () {
   const latest = await DatabaseHandler.fetchLatest();
   const tweets = await TweetHandler.fetchNewTweets(latest && latest.tweet.id);
 
+  console.log(`${tweets.length} new mentions`);
+
   if (tweets.length > 0) {
     const parsedTweets = TweetHandler.parseTweets(tweets);
 
@@ -30,7 +32,35 @@ setInterval(async function () {
     await Promise.all(sender);
 
     await DatabaseHandler.saveReminders(parsedTweets);
+    console.log('Tweet(s) sent and saved to the database for later reminding');
   }
 
-  console.log(`${tweets.length} new mentions`);
-}, 2000);
+  const pastReminders = await DatabaseHandler.getReminders();
+  if (pastReminders.length > 0) {
+    const tweetSender = [];
+    const dbSender = [];
+    pastReminders.forEach((reminder) => {
+      tweetSender.push(
+        Queue.add(SendTweet.key, {
+          message: `Ola! Aqui esta o seu lembrete para este status!`,
+          reminder: {
+            tweet: reminder.tweet,
+            requester: reminder.requester,
+          },
+        })
+      );
+
+      dbSender.push(DatabaseHandler.setAsDone(reminder.id));
+    });
+
+    await Promise.all([tweetSender, dbSender]);
+
+    console.log(
+      `${pastReminders.length} reminders sent and updated "done" to TRUE in the db`
+    );
+  } else {
+    console.log('0 reminders to be sent');
+  }
+
+  console.log(`\n`);
+}, 60000);
