a2d280487d14fd3afae8604d5826851ffff413f5
Bruno De Masi <brunodemasi1@gmail.com>
2020-04-01 20:53:55 -0400

    feat: listening and replying to all new mentions

--- src/app.js
+++ src/app.js
@@ -1,25 +1,36 @@
-import TweetHandler from './app/Handlers/TweetHandler';
-import DateHandler from './app/Handlers/DateHandler';
+import TweetHandler from './app/handlers/TweetHandler';
+import DateHandler from './app/handlers/DateHandler';
 
-import DatabaseHandler from './app/Handlers/DatabaseHandler';
+import SendTweet from './app/jobs/SendTweet';
+import Queue from './lib/Queue';
+
+import DatabaseHandler from './app/handlers/DatabaseHandler';
 import './database';
 
 setInterval(async function () {
   const latest = await DatabaseHandler.fetchLatest();
   const tweets = await TweetHandler.fetchNewTweets(latest && latest.tweet.id);
 
-  const parsedTweets = TweetHandler.parseTweets(tweets);
-  // const formattedDate = DateHandler.formatResponseDate(
-  //   parsedTweets[0].parsed_date
-  // );
+  if (tweets.length > 0) {
+    const parsedTweets = TweetHandler.parseTweets(tweets);
 
-  // // TODO: redis
-  // TweetHandler.replyTweet({
-  //   username: parsedTweets[0].requester.name,
-  //   tweet_id: parsedTweets[0].tweet.id,
-  //   message: `Ok! Seu lembrete foi agendado para ${formattedDate}! Até lá!`,
-  // });
+    const sender = [];
+    parsedTweets.forEach(({ tweet, requester, parsed_date }) => {
+      const date = DateHandler.formatResponseDate(parsed_date);
+      sender.push(
+        Queue.add(SendTweet.key, {
+          message: `Ok! Seu lembrete foi agendado para ${date}! Até lá!`,
+          reminder: {
+            tweet,
+            requester,
+          },
+        })
+      );
+    });
+    await Promise.all(sender);
 
-  // await DatabaseHandler.saveReminders(parsedTweets);
-  // console.log(latest);
-}, 2000);
+    await DatabaseHandler.saveReminders(parsedTweets);
+  }
+
+  console.log(`${tweets.length} new mentions`);
+}, 60000);
